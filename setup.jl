92ee6d80c1f5f3fe3f666b0bb72b9546fb23a520
Abhinav Natarajan <abhinav.v.natarajan@gmail.com>
2023-02-06 19:40:18 +0000

    updated setup.jl to check for salso

--- setup.jl
+++ setup.jl
@@ -6,4 +6,11 @@ ENV["R_HOME"] = "C:/Program Files/R/R-4.2.2/"
 
 Pkg.add(url="https://github.com/jwmi/BayesianMixtures.jl") # this is an unregistered dependency, so we include it here until Julia is updated to allow such dependencies
 Pkg.instantiate()
+using RCall: rcopy, @R_str
+salso_is_installed = rcopy(R"""
+    require(salso)
+    """)
+salso_is_installed || error("The R package salso could not be loaded.")
+
 @info "Successfully set up environment!"
+
